9442a832dae7390e9ce38e89a0cf0b7d227a6473
loclink <loclink@163.com>
2023-12-29 15:35:20 +0800

    fix: 修复侧边栏透明状态

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,7 +2,15 @@ return {
   {
     "folke/tokyonight.nvim",
     lazy = true,
-    opts = { style = "storm", transparent = true },
+    opts = {
+      style = "storm",
+      transparent = true,
+      transparent_sidebar = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
   },
   {
     "LazyVim/LazyVim",
