35419cf5db86c54c419177b4928c81ddc8e5f81f
loclink <loclink@163.com>
2023-12-29 15:36:42 +0800

    fix: 取消全选功能键映射

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,7 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local Util = require("lazyvim.util")
-local map = Util.safe_keymap_set
-
-map("n", "<c-a>", "ggVG", { desc = "全选", noremap = true, silent = true })
+-- local Util = require("lazyvim.util")
+-- local map = Util.safe_keymap_set
+-- map("n", "<c-a>", "ggVG", { desc = "全选", noremap = true, silent = true })
