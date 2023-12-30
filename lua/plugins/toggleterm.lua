707231975af4f7bc5f7002cd65d8af2ef413ee77
loclink <loclink@163.com>
2023-12-30 20:01:44 +0800

    feat: 加入内置终端

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,10 +1,21 @@
 -- 内置终端
 return {
   "akinsho/toggleterm.nvim",
-  version = "*",
-  opts = {--[[ things you want to change go here]]
-  },
   config = function()
-    require("toggleterm").setup({})
+    require("toggleterm").setup({
+      open_mapping = [[<c-t>]],
+      shade_terminals = false,
+      shell = "zsh --login",
+    })
   end,
+
+  keys = {
+    { [[<C-t>]] },
+    { "<leader>0", "<Cmd>2ToggleTerm<Cr>", desc = "Terminal #2" },
+    {
+      "<leader>td",
+      "<cmd>ToggleTerm size=40 dir=~/Desktop direction=horizontal<cr>",
+      desc = "Open a horizontal terminal at the Desktop directory",
+    },
+  },
 }
